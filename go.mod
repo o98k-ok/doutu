ddb78436d9d3a9e047704a8358f4f92651e19261
o98k-ok <hggend@gmail.com>
2024-07-23 12:20:24 +0800

    [feature]<main>: finish v1 ✨✨

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/o98k-ok/doutu
+
+go 1.22.0
+
+require github.com/o98k-ok/lazy v0.2.51
+
+require (
+	golang.org/x/exp/shiny v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/image v0.14.0 // indirect
+	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/duke-git/lancet v1.4.3
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.4.0 // indirect
+	golang.design/x/clipboard v0.7.0
+	howett.net/plist v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/duke-git/lancet/netutil"
+	"github.com/o98k-ok/lazy/v2/alfred"
+	"golang.design/x/clipboard"
+)
+
+var (
+	CachePathKey = "cache_path"
+)
+
+type ResponseList []struct {
+	Path   string `json:"path"`
+	Width  int    `json:"width"`
+	HashID string `json:"hashId"`
+	Height int    `json:"height"`
+}
+
+func query(key string) ResponseList {
+	dest := url.QueryEscape(key)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://www.dbbqb.com/api/search/json?start=0&w="+dest, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+	req.Header.Set("Client-Id", "")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "Hm_lvt_7d2469592a25c577fe82de8e71a5ae60=1721633603; HMACCOUNT=4873E08E7D2507F8; Hm_lpvt_7d2469592a25c577fe82de8e71a5ae60=1721633615")
+	req.Header.Set("Referer", "https://www.dbbqb.com/s?w="+dest)
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+	req.Header.Set("Web-Agent", "web")
+	req.Header.Set("sec-ch-ua", `"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"`)
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var list ResponseList
+	json.Unmarshal(bodyText, &list)
+	return list
+}
+
+func main() {
+	envs, err := alfred.FlowVariables()
+	if err != nil {
+		alfred.InputErrItems("read env failed " + err.Error()).Show()
+		return
+	}
+	path := envs[CachePathKey]
+	if len(path) == 0 {
+		path = "./data"
+	}
+	os.MkdirAll(path, 0755)
+
+	cli := alfred.NewApp("doutu utils")
+	cli.Bind("get", func(s []string) {
+		items := alfred.NewItems()
+
+		resList := query(s[0])
+		length := len(resList)
+		if length > 20 {
+			length = 20
+		}
+
+		groups := sync.WaitGroup{}
+		groups.Add(length)
+		for i := 0; i < length; i++ {
+			filename := path + "/" + strings.ReplaceAll(resList[i].Path, "/", "_") + ".jpg"
+
+			go func(index int, file string) {
+				defer groups.Done()
+				url := fmt.Sprintf("https://image.dbbqb.com/%s", resList[index].Path)
+				netutil.DownloadFile(file, url)
+			}(i, filename)
+
+			item := alfred.NewItem(fmt.Sprintf("图片 %d", i), "", filename)
+			item.Icon = &alfred.Icon{}
+			item.WithIcon(filename)
+			items.Append(item)
+		}
+		groups.Wait()
+		items.Show()
+	})
+
+	cli.Bind("copy", func(s []string) {
+		d, _ := os.ReadFile(s[0])
+		clipboard.Write(clipboard.FmtImage, d)
+	})
+
+	cli.Run(os.Args)
+}
